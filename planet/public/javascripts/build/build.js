4f532f0491b03f084f31f5c45ef96f76a53876d5
Kevin Roark <ker2143@columbia.edu>
2015-06-29 23:15:34 -0700

    add the test image for the new lol character

--- planet/public/javascripts/build/build.js
+++ planet/public/javascripts/build/build.js
@@ -16557,22 +16557,17 @@ function Avatar(options) {
   this.twitching = false;
   this.sleeping = false;
 
+  this.faceMaterial = new THREE.MeshBasicMaterial({
+    map: THREE.ImageUtils.loadTexture('')
+  });
+
   SheenModel.call(this, options);
 }
-
-Avatar.prototype.createFaceMaterial = function(texture) {
-  this.faceMaterial = new THREE.MeshBasicMaterial({
-    map: texture
-  });
-};
 
 Avatar.prototype.loadMesh = function(callback) {
   var self = this;
 
   this.faceGeometry = new THREE.BoxGeometry(2, 2, 2);
-  if (!this.faceMaterial) {
-    this.createFaceMaterial(THREE.ImageUtils.loadTexture(this.faceImageUrl || ''));
-  }
   this.faceMesh = new THREE.Mesh(this.faceGeometry, this.faceMaterial);
 
   loader('/javascripts/3d_models/body.js', function (geometry, materials) {
@@ -16671,13 +16666,8 @@ Avatar.prototype.updateFaceImage = function(image) {
 
   if (texture) {
     texture.needsUpdate = true;
-
-    if (this.faceMaterial) {
-      this.faceMaterial.map = texture;
-      this.faceMaterial.needsUpdate = true;
-    } else {
-      this.createFaceMaterial(texture);
-    }
+    this.faceMaterial.map = texture;
+    this.faceMaterial.needsUpdate = true;
   }
 };
 
