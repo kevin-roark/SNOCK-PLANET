cd9df10921c9302201c75b69dd5fae3deefda23a
Kevin Roark <ker2143@columbia.edu>
2015-07-11 15:49:15 -0700

    add caching back to model-loader with much success

--- planet/emitter.js
+++ planet/emitter.js
@@ -6,7 +6,7 @@ var planetState = require('./planet-state');
 var redisClient = redis.createClient();
 var io = socketIOEmitter(redisClient, {key: 'planet'});
 
-console.log("\nEmitter is ready to emit for u ...\n");
+console.log("\nEmitter is ready to emit for u ...");
 
 setInterval(function() {
   planetState.getState(function(state) {
