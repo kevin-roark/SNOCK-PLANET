57819dbf8724d87cc0ba9bf5447d0175e2d70cb3
Kevin Roark <ker2143@columbia.edu>
2015-07-19 17:15:47 -0700

    new page title

--- planet/routes/index.js
+++ planet/routes/index.js
@@ -9,7 +9,10 @@ var ioURL = process.env.PLANET_IO_URL || 'http://localhost:' + ioPort;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SNOCK PLANET', ioURL: ioURL });
+  res.render('index', {
+    title: 'NEW (SNOCK) PLANET — A place to call and be your own. Realtime chat by Kevin Roark',
+    ioURL: ioURL
+  });
 });
 
 /* POST FACE IMAGE DATA */
